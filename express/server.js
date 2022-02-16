4819ac6ee7d1ae5f01c68091ca5b6bb8265e3a25
elie-atia <elie-atia@hotmail.fr>
2022-02-16 21:59:40 +0200

    ssss10

--- express/server.js
+++ express/server.js
@@ -14,14 +14,14 @@ router.get('/', (req, res) => {
 });
 
 router.post('/ordersHistory', (req, res) => {
-  const {signature} = req.query;
-   //var pairs = body.split('&');
+  const { signature, key, symbol, timestamp } = req.query;
+  //var pairs = body.split('&');
   // var result = {};
   // pairs.forEach(function (pair) {
   //   pair = pair.split('=');
   //   result[pair[0]] = decodeURIComponent(pair[1] || '');
   // });
-  res.send(`The signature of the request is: ${signature}`);
+  res.send(`signature: ${signature} key: ${key} symbol: ${symbol} timestamp: ${timestamp}`);
 });
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
