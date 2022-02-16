d2c9182e311d4d5f657014b8d811716b33eb7dd2
elie-atia <elie-atia@hotmail.fr>
2022-02-16 19:06:03 +0200

    lll

--- express/server.js
+++ express/server.js
@@ -12,15 +12,12 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js This server was deploy by elie!</h1>');
   res.end();
 });
-router.get('/test1', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello. I have had this new endpoint. Enjoy !</h1>');
-  res.end();
-});
 
-router.post('/test1', (req, res) => res.json( JSON.stringify(req.body)));
-router.post('/test2', (req, res) => res.send(req.body));
-router.post('/test3', (req, res) => res.send( JSON.stringify(req.body)));
+
+router.post('/ordersHistory', (req, res) =>{
+const {  signature, key, totalParams } = req.body;
+ res.send(req.body);
+});
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
