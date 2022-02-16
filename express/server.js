ff7de743d677df1e16ad321ad0cf561c0333b8e8
elie-atia <elie-atia@hotmail.fr>
2022-02-16 22:06:48 +0200

    uuuu

--- express/server.js
+++ express/server.js
@@ -11,12 +11,12 @@ app.use(bodyParser.json());
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 app.use(
-    cors({
-      origin: '*',
-      //origin: ['http://localhost:3000', 'https://www.binance.com/api','https://react-binance-app.web.app'],
-    }),
-  );
-  
+  cors({
+    origin: '*',
+    //origin: ['http://localhost:3000', 'https://www.binance.com/api','https://react-binance-app.web.app'],
+  }),
+);
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js This server was deploy by elie!</h1>');
@@ -25,13 +25,29 @@ router.get('/', (req, res) => {
 
 router.post('/ordersHistory', (req, res) => {
   const { signature, key, symbol, timestamp } = req.query;
-  //var pairs = body.split('&');
-  // var result = {};
-  // pairs.forEach(function (pair) {
-  //   pair = pair.split('=');
-  //   result[pair[0]] = decodeURIComponent(pair[1] || '');
-  // });
-  res.send(`signature: ${signature} key: ${key} symbol: ${symbol} timestamp: ${timestamp}`);
+
+  axios.get(`https://api.binance.com/api/v3/allOrders?symbol=${symbol}&timestamp=${timestamp}&signature=${signature}`
+    , {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MBX-APIKEY': key,
+        'Host': 'api.binance.com',
+      }
+    }
+  )
+    .then(response => {
+      // res.status(200).json({
+      //   data: JSON.parse(JSON.stringify(response.data)),
+      // });
+      res.send("axios succes");
+    })
+    .catch(error => {
+      // res.status(403).json({
+      //   my_msg: "an error occurs in the axios request. Check the request and try again",
+      //   error: error
+      // })
+      res.send("axios failed");
+    });
 });
 
 app.use('/.netlify/functions/server', router);  // path must route to lambda
