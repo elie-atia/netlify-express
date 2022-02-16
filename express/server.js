908d68b009dd0a8ae563ee27e9a594c210d45993
elie-atia <elie-atia@hotmail.fr>
2022-02-16 16:57:50 +0200

    fix test1 (maybe)

--- express/server.js
+++ express/server.js
@@ -11,11 +11,15 @@ router.get('/', (req, res) => {
   res.write('<h1>Hello from Express.js This server was deploy by elie!</h1>');
   res.end();
 });
-app.get('/test1', function (req, res) {
-  res.send("hello i have add this new endpoint. It is easy !")
+router.get('/test1', (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.write('<h1>Hello. I have had this new endpoint. Enjoy !</h1>');
+  res.end();
 });
+
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.post('/test1', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
