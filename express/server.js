1fab6c027bd98d95e8ab12e81baec0957a37c8a4
elie-atia <elie-atia@hotmail.fr>
2022-02-16 17:05:39 +0200

    atemot to use axios

--- express/server.js
+++ express/server.js
@@ -4,15 +4,50 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const axios = require('axios');
 
 const router = express.Router();
+
+app.use(cors({ origin: true }));
+app.use(
+  cors({
+    origin: '*',
+    //origin: ['http://localhost:3000', 'https://www.binance.com/api','https://react-binance-app.web.app'],
+  }),
+);
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js This server was deploy by elie!</h1>');
   res.end();
 });
 router.get('/test1', (req, res) => {
-  res.send('Hello. I have had this new endpoint with res.send(). Enjoy !');
+  const { signature, key, symbol, timestamp } = req.body;
+  console.log(JSON.stringify(req.body));
+
+  axios.get(`https://api.binance.com/api/v3/allOrders?symbol=${symbol}&timestamp=${timestamp}&signature=${signature}`
+    , {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-MBX-APIKEY': key,
+        'Host': 'api.binance.com',
+      }
+    }
+  )
+    .then(response => {
+      console.log("AXIOS REQUEST WORKS FINE");
+      res.status(200).json({
+        data: JSON.parse(JSON.stringify(response.data)),
+      });
+    })
+    .catch(error => {
+      console.log("AXIOS GET ERROR: ", error);
+      res.status(403).json({
+        my_msg: "an error occurs in the axios request. Check the request and try again",
+        error: error
+      })
+    });
 });
 
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
