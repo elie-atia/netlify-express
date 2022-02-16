c87fc1df8d0e6654855e0c499d8526c2ee81b12f
elie-atia <elie-atia@hotmail.fr>
2022-02-16 18:51:32 +0200

    fff

--- express/server.js
+++ express/server.js
@@ -3,8 +3,10 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
 const router = express.Router();
+
+app.use(bodyParser.json());
+
 router.get('/', (req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
   res.write('<h1>Hello from Express.js This server was deploy by elie!</h1>');
@@ -16,11 +18,8 @@ router.get('/test1', (req, res) => {
   res.end();
 });
 
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
-router.post('/test1', (req, res) => res.json({ postBody: req.body }));
+router.post('/test1', (req, res) => res.json({ postBody: JSON.stringify(req.body) }));
 
-app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 app.use('/test1', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
